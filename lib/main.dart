2f91af5913bd950c41ac182435f1e19ea8cd26a6
MariaLPF <liriopf@gmail.com>
2020-04-13 12:43:14 +0200

    Creacion esquema de la aplicación

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_qr_scanner/src/pages/addresses_page.dart';
 import 'package:flutter_qr_scanner/src/pages/home_page.dart';
+import 'package:flutter_qr_scanner/src/pages/maps_page.dart';
  
 void main() => runApp(MyApp());
  
@@ -11,8 +13,13 @@ class MyApp extends StatelessWidget {
       title: 'QRReader App',
       initialRoute: 'home',
       routes: {
-        'home' : (BuildContext context) => HomePage()
-      }
+        'home'         : (BuildContext context) => HomePage(),
+        //'mapsPage'     : (BuildContext context) => MapsPage(),
+        //'addressesPage': (BuildContext context) => AddressesPage(),
+      },
+      theme: ThemeData(
+        primaryColor: Colors.deepPurple
+      )
     );
   }
 }
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,13 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_qr_scanner/src/pages/addresses_page.dart';
+import 'package:flutter_qr_scanner/src/pages/maps_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola home page'),
+      appBar: AppBar(
+        title: Text('QR Scanner'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.delete_forever), 
+            onPressed: (){},
+          )
+        ],
       ),
+      body: _callPage(currentIndex),
+      bottomNavigationBar: _createBottomNavigationBar(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.filter_center_focus),
+        onPressed: (){
+
+        },
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+    );
+  }
+
+  Widget _callPage(int currentPage) {
+    switch(currentPage){
+
+      case 0: return MapsPage();
+      case 1: return AddressesPage();
+
+      default : return MapsPage();
+    }
+  }
+
+  Widget _createBottomNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: currentIndex, //Indica elemento activo
+      onTap: (index){
+        setState(() {
+          currentIndex = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(icon:Icon(Icons.map), title: Text('Mapas')),
+        BottomNavigationBarItem(icon:Icon(Icons.brightness_5), title: Text('Direcciones'))
+      ],
+
     );
   }
 }
